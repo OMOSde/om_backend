0e3e0690b5e5b5a4d7ead768c821909d5257a8d7
Rene Fehrmann <rene.fehrmann@omos.de>
2015-06-19 19:22:41 +0200

    New toggle button for backend links

--- dca/tl_om_backend_links.php
+++ dca/tl_om_backend_links.php
@@ -77,6 +77,13 @@ $GLOBALS['TL_DCA']['tl_om_backend_links'] = array
 				'icon'                => 'delete.gif',
 				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['tl_faq_category']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
 			),
+            'toggle' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_om_backend_links']['toggle'],
+                'icon'                => 'visible.gif',
+                'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+                'button_callback'     => array('tl_om_backend_links', 'toggleIcon')
+            ),
 			'show' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_om_backend_links']['show'],
@@ -135,9 +142,10 @@ $GLOBALS['TL_DCA']['tl_om_backend_links'] = array
     (
       'label'                   => &$GLOBALS['TL_LANG']['tl_om_backend_links']['published'],
       'inputType'               => 'checkbox',
+      'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
       'sql'                     => "char(1) NOT NULL default ''"
-    )
 	)
+  )
 );
 
 
@@ -172,4 +180,81 @@ class tl_om_backend_links extends Backend
   {
     return $row['be_group'] . ' - ' . $row['language'];
   }
+
+    /**
+     * Return the "toggle visibility" button
+     * @param array
+     * @param string
+     * @param string
+     * @param string
+     * @param string
+     * @param string
+     * @return string
+     */
+    public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
+    {
+        if (strlen(Input::get('tid')))
+        {
+            $this->toggleVisibility(Input::get('tid'), (Input::get('state') == 1));
+            $this->redirect($this->getReferer());
+        }
+
+        // Check permissions AFTER checking the tid, so hacking attempts are logged
+        if (!$this->User->isAdmin && !$this->User->hasAccess('tl_om_backend_links::published', 'alexf'))
+        {
+            return '';
+        }
+
+        $href .= '&amp;tid='.$row['id'].'&amp;state='.($row['published'] ? '' : 1);
+
+        if (!$row['published'])
+        {
+            $icon = 'invisible.gif';
+        }
+
+        return '<a href="'.$this->addToUrl($href).'" title="'.specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ';
+    }
+
+
+    /**
+     * Disable/enable an backend link
+     * @param integer
+     * @param boolean
+     */
+    public function toggleVisibility($intId, $blnVisible)
+    {
+        // Check permissions to publish
+        if (!$this->User->isAdmin && !$this->User->hasAccess('tl_om_backend_links::published', 'alexf'))
+        {
+            $this->log('Not enough permissions to publish/unpublish backend link ID "'.$intId.'"', __METHOD__, TL_ERROR);
+            $this->redirect('contao/main.php?act=error');
+        }
+
+        $objVersions = new Versions('tl_om_backend_links', $intId);
+        $objVersions->initialize();
+
+        // Trigger the save_callback
+        if (is_array($GLOBALS['TL_DCA']['tl_om_backend_links']['fields']['published']['save_callback']))
+        {
+            foreach ($GLOBALS['TL_DCA']['tl_om_backend_links']['fields']['published']['save_callback'] as $callback)
+            {
+                if (is_array($callback))
+                {
+                    $this->import($callback[0]);
+                    $blnVisible = $this->$callback[0]->$callback[1]($blnVisible, $this);
+                }
+                elseif (is_callable($callback))
+                {
+                    $blnVisible = $callback($blnVisible, $this);
+                }
+            }
+        }
+
+        // Update the database
+        $this->Database->prepare("UPDATE tl_om_backend_links SET tstamp=". time() .", published='" . ($blnVisible ? 1 : '') . "' WHERE id=?")
+            ->execute($intId);
+
+        $objVersions->create();
+        $this->log('A new version of record "tl_om_backend_links.id='.$intId.'" has been created'.$this->getParentEntries('tl_om_backend_links', $intId), __METHOD__, TL_GENERAL);
+    }
 }
